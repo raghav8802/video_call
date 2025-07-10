0624842ba603de50fe344acaadcc4a1c8e26d46c
anant raghav <anantraghav@anants-MacBook-Pro.local>
2025-07-10 16:35:19 +0530

    yoom change to gopro connect

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling App",
+  title: "GoPro Connect",
+  description: "GoPro Connect",
   icons: {
     icon: "/icons/logo.svg",
   },
