c943eda73edbc1e82916c7f1df3d0779fe0f996e
anant raghav <anantraghav@anants-MacBook-Pro.local>
2025-07-10 16:24:15 +0530

    logo size

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -17,8 +17,8 @@ const Home = () => {
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            <img src="/icons/gopro.png" alt="GoPro" className="w-8 h-8 mx-auto" />
+          <h2 className="">
+          <img src="/icons/gopro.png" alt="GoPro" className="w-200 h-40 mx-auto max-w-[273px] rounded py-2 text-center text-base font-normal"/>
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
