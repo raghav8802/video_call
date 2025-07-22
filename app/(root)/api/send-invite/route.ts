caeb75a03a32144304b9d81d7f356784739d058d
anant raghav <anantraghav@anants-MacBook-Pro.local>
2025-07-22 14:43:51 +0530

    fix

--- app/(root)/api/send-invite/route.ts
+++ app/(root)/api/send-invite/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
     }
 
-    const { data, error } = await resend.emails.send({
+    const {  error } = await resend.emails.send({
       from: 'GoPro Support <support@goproibpl.com>',
       to,
       subject: 'You are invited to a meeting!',
